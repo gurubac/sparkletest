86b69920eb6331e8096866f8b083a1dd8e5e1afc
gurubac <gbrbachchu@gmail.com>
2023-01-20 14:38:11 -0800

    init

---
+++ sparkletest/sparkletestApp.swift
@@ -0,0 +1,17 @@
+//
+//  sparkletestApp.swift
+//  sparkletest
+//
+//  Created by guru on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct sparkletestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

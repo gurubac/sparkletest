b1d75077a909b5962919e7edf1236d411ccbaccd
gurubac <gbrbachchu@gmail.com>
2023-01-21 18:43:34 -0800

    add teamid retrieving instead of harcoded one in create_dmg.sh

--- create_dmg.sh
+++ create_dmg.sh
@@ -2,10 +2,11 @@
 ./archive.sh
 
 ARCHIVE_APP_DIR="$HOME/Desktop/sparkletest"
+TEAM_ID=$(security find-identity -v -p codesigning | grep "Apple Development" | awk -F'[()]' '{print $3}')
 
 # Create the DMG, from build directory and put it in the destination directory
 create-dmg \
     --volname "sparkletest" \
-    --codesign "8S6PWN6A5K" \
+    --codesign $TEAM_ID \
     "$ARCHIVE_APP_DIR/sparkletest.dmg" \
     "$ARCHIVE_APP_DIR/sparkletest.app"

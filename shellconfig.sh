f248b7b40620e0ed116509d53f317ba3538d31c1
gurubac <gbrbachchu@gmail.com>
2023-01-22 21:46:26 -0800

    update appcast

--- shellconfig.sh
+++ shellconfig.sh
@@ -1,5 +1,4 @@
 # Description: This file contains the configuration for the shell scripts
-
 ##############################################
 # Xcode configuration
 # APP_VERSION is the version number of the app (This is the only thing you should change, the rest of the file can be left as is unless you want to change the app name or the destination directory for the archive)
@@ -16,6 +15,12 @@ TEAM_ID=$(security find-identity -v -p codesigning | grep "Apple Development" | 
 EXPORT_OPTIONS_PLIST="exportOptions.plist"
 
 ##############################################
+# Github configuration for generate_appcast.sh 
+GITHUB_USER="gurubac"
+
+RELEASE_FILE_EXTENSION="dmg"
+
+##############################################
 # Sparkle config
 # Gets the path of the generate_appcast binary which is in ~/Library/Developer/Xcode/DerivedData/sparkletest-fqkskzevuwuhmcfjetohllfhqzis/SourcePackages/artifacts/sparkle/bin (the stuff after sparkletest- is a random string)
 SPARKLE_BIN_DIR=$(find ~/Library/Developer/Xcode/DerivedData -name generate_appcast -type f -print0 | awk -F/ '{for(i=1;i<=NF;i++){if($i == "bin"){for(j=1;j<=i;j++){printf("%s/",$j)};break}};print ""}')
@@ -29,8 +34,3 @@ SPARKLE_RELEASE_NOTES_LINK="https://github.com/$GITHUB_USER/$APP_NAME/releases/t
 # For full release notes
 SPARKLE_FULL_RELEASE_NOTES_LINK="https://github.com/$GITHUB_USER/$APP_NAME/releases"
 
-##############################################
-# Github configuration for generate_appcast.sh 
-GITHUB_USER="gurubac"
-
-RELEASE_FILE_EXTENSION="dmg"

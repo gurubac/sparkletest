ee8a2b2df59603c8a4f10f01add8acf7b286ab67
gurubac <gbrbachchu@gmail.com>
2023-01-22 22:55:42 -0800

    almost works now just some GITHUB_TOKEN issue

---
+++ deploy_release.sh
@@ -0,0 +1,29 @@
+source shellconfig.sh
+
+./generate_appcast.sh
+
+export GITHUB_TOKEN=...
+
+# Create a new tag for the release and push it to GitHub
+git tag -a v$APP_VERSION -m "v$APP_VERSION" && git push --tags
+
+# Commit the changes to the appcast.xml file and push to GitHub
+git add appcast.xml
+git commit -m "Update appcast.xml to v$APP_VERSION"
+git push
+
+# Create a new release on GitHub
+github-release release \
+    --tag v$APP_VERSION \
+    --user $GITHUB_USER \
+    --repo $APP_NAME \
+    --name "v$APP_VERSION" \
+    --description "Release v$APP_VERSION. This is an automatic release created by the deploy_release.sh script. Check back later for updated release notes."
+
+# Upload the archive to the release
+github-release upload \
+    --tag v$APP_VERSION \
+    --user $GITHUB_USER \
+    --repo $APP_NAME \
+    --name "$APP_NAME.$RELEASE_FILE_EXTENSION" \
+    --file "$ARCHIVE_APP_DIR/$APP_NAME.$RELEASE_FILE_EXTENSION"
--- shellconfig.sh
+++ shellconfig.sh
@@ -1,8 +1,9 @@
 # Description: This file contains the configuration for the shell scripts
+
 ##############################################
 # Xcode configuration
 # APP_VERSION is the version number of the app (This is the only thing you should change, the rest of the file can be left as is unless you want to change the app name or the destination directory for the archive)
-APP_VERSION="1.0.1"
+APP_VERSION="1.0.3"
 
 # Set the app name and destination directory for the archive
 APP_NAME="sparkletest"
@@ -17,16 +18,15 @@ EXPORT_OPTIONS_PLIST="exportOptions.plist"
 ##############################################
 # Github configuration for generate_appcast.sh 
 GITHUB_USER="gurubac"
-
 RELEASE_FILE_EXTENSION="dmg"
 
 ##############################################
 # Sparkle config
-# Gets the path of the generate_appcast binary which is in ~/Library/Developer/Xcode/DerivedData/sparkletest-fqkskzevuwuhmcfjetohllfhqzis/SourcePackages/artifacts/sparkle/bin (the stuff after sparkletest- is a random string)
-SPARKLE_BIN_DIR=$(find ~/Library/Developer/Xcode/DerivedData -name generate_appcast -type f -print0 | awk -F/ '{for(i=1;i<=NF;i++){if($i == "bin"){for(j=1;j<=i;j++){printf("%s/",$j)};break}};print ""}')
-
 # Directory to generate the appcast.xml file to
 APPCAST_DIR=$(pwd) # This is the current directory
+
+# Gets the path of the generate_appcast binary which is in ~/Library/Developer/Xcode/DerivedData/sparkletest-fqkskzevuwuhmcfjetohllfhqzis/SourcePackages/artifacts/sparkle/bin (the stuff after sparkletest- is a random string)
+SPARKLE_BIN_DIR=$(find ~/Library/Developer/Xcode/DerivedData -name generate_appcast -type f -print0 | awk -F/ '{for(i=1;i<=NF;i++){if($i == "bin"){for(j=1;j<=i;j++){printf("%s/",$j)};break}};print ""}')
 
 # For version specific release notes
 SPARKLE_RELEASE_NOTES_LINK="https://github.com/$GITHUB_USER/$APP_NAME/releases/tag/v$APP_VERSION"
